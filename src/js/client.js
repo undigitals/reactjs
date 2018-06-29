4a8041fbd07edf8483bb6b4bb7364d52012cb51b
Umid Negmatullayev <undigitals@gmail.com>
2018-06-30 01:16:39 +0900

    these are states, and state gets used for one component

--- src/js/client.js
+++ src/js/client.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import Layout from "./components/Layout"; 
-import Header from "./components/Header";
-
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -4,9 +4,18 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 export default class Layout extends React.Component{
+    constructor(){
+        super();
+        this.state = {name: "Will"};
+    }
+
     render(){
+        setTimeout(() => {
+            this.setState({name:"Bob"});
+        }, 1000);
         return (
             <div>
+                {this.state.name}
                 <Header/>
                 <Footer/>
             </div>
